61d44434d77aefc9683fbba65bb2b0127d075b84
z1r0666 <3427303898@qq.com>
2025-11-20 02:07:23 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+function showMessage() {
+    document.getElementById('popup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('popup').style.display = 'none';
+}
+
+// 小游戏：气球
+function popBalloon() {
+    alert("🎉 生日祝福送给你！祝你生日快乐！");
+}
+
+// 二维码生成
+var qrcode = new QR
